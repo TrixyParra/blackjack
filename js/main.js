983d818e04fc17a8127873e55ffa35cc0f3e7f65
TrixyParra <trixy.parra@gmail.com>
2022-11-15 03:04:47 -0800

    JS - added all global variables and notes for functions to be created.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 // ToDo: Blackjack Game 
+
+// * Global Variables 
+let gameisAlive = false; 
+let hasBlackjack = false; 
+let messageDisplayed = document.getElementById("message"); 
+let cardsDrawn = document.getElementById("cards-drawn"); 
+let sumOfCards = document.getElementById("sum"); 
+let playerInfo = document.getElementById("player-info"); 
+let player = {
+    name: "Player", 
+    chips: 200 
+}; 
+let message = ""; 
+let cards = []; 
+let  sum = 0; 
+
+// * Player Info 
+playerInfo.textContent = player.name + ": $" + player.chips 
+
+// * Get Random Number Function 
+
+// * Start Game Function 
+
+// * Render game Function 
+
+// * New Card Function 
